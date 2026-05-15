8b802d368493e880ba6c8982efc877e77ba16bc4
gobdanov <gobdanov@inbox.ru>
2026-05-15 11:53:00 +0500

    разработанные модели данных. богданов

---
+++ pr33_2/Data/Models/Items.cs
@@ -0,0 +1,13 @@
+﻿namespace pr33_2.Data.Models
+{
+    public class Items
+    {
+        public int Id {get;set;}
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Img { get; set; }
+        public int Price { get; set; }
+        public Categories category { get; set; }
+
+    }
+}
